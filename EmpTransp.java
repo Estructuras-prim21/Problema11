a3750fab80a5422f7a81d8e09b4af961c9b699fd
ximerlaris <78515361+ximerlaris@users.noreply.github.com>
2021-02-03 20:40:45 -0600

    Create EmpTransp.java

--- EmpTransp.java
+++ EmpTransp.java
@@ -25,7 +25,6 @@ public class EmpTransp {
         this.nombre = nombre;
     }
     
-    //3
     public String estaDisponible(String placas){
         int i=0;
         boolean encontre=false;
@@ -53,27 +52,27 @@ public class EmpTransp {
          else
              return ("No existe");
         }
-        //pregunta 1
-      public String rentaCamionTuristas(int totalPasaj, double km){
-       StringBuilder sb=new StringBuilder();
-       int i;
-       
-       /*i=0;
-       while(i<ocupados && camiones[i].getTotalPasaj()!=totalPasaj)
-           i++;
-       
-       if(i<ocupados && camiones[i]instanceof Turismo){*/
-           for(i=0;i<ocupados;i++)
-               if(camiones[i]instanceof Turismo && camiones[i].getTotalPasaj()==totalPasaj){
-               sb.append(((Turismo)camiones[i]).toString()+"\n");
-               sb.append(" ");
-               sb.append("El costo del servicio es de: $"+((Turismo)camiones[i]).calculaCostoServicio(km));
-               }
-           
-       
-       return sb.toString();
-   }
-       
+        
+     //2))
+    public boolean indicaCupoEscolares(int cantN, int cantC){ //cant camiones y cant niños
+        boolean resp=false;
+        int i, contC=0, contN=0;
+        
+        for(i=0;i<totalCam;i++)
+            if(camiones[i]instanceof Escolar){
+                contC++;
+                if (camiones[i].getTotalPasj()>= cantN)
+                    contN++; //si la capacidad de pasajeros es mayor a la que piden, aumenta el contador de camiones que pueden llevar esa capacidad      
+            }
+        
+        if(contC >= cantC && contN >= cantN){
+            resp=true;
+        }
+        
+        return resp;
+        
+    }   
+    
         //pregunta 4
     public String escolarProy20 (){
         String resp= "No hay camiones escolares que lleven mas de 20 alumnos y tengan espacio para proyectos";
@@ -88,7 +87,24 @@ public class EmpTransp {
         return resp;
     }
     
+   
+    //5
+    public double totalCamiones(String marca, double kilometros, double costo){
+        double contador = 0;
+        int posicion=-1;
+        int i=0;
+        boolean resp=false;
         
+        while(i<totalCam && !camiones[i].getMarca().equalsIgnoreCase(marca)){
+           posicion = i;
+       }
+        if(i<totalCam && camiones[i]instanceof Turismo){
+         resp=true;
+             if(((Turismo)camiones[posicion]).hayServiBar() && ((Turismo)camiones[posicion]).calculaCostoServicio(kilometros)< costo){
+            contador ++;
+             }
+         }  
+       return contador;
     }
     
     
