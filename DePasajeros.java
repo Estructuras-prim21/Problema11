583401a987503bea566935c07836f6943053da5b
anapaulaaviles <78515655+anapaulaaviles@users.noreply.github.com>
2021-02-03 20:11:55 -0600

    Update DePasajeros.java

--- DePasajeros.java
+++ DePasajeros.java
@@ -36,6 +36,10 @@ public abstract class DePasajeros extends Camion {
         return totalPasj;
     }
     
+     public double getCostoCamionDePasajeros() {
+        return getCostoCamion();
+    }
+    
     
 
     
